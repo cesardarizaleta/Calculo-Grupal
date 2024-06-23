909ce89137e1ebaba722fb41264128063d3c52a9
alejandrochmejia <alejandrochmejia@gmail.com>
2024-06-22 21:23:59 -0400

    Eliminacion por Gauss terminada

--- Proyectos/MontañaRusa/ecuaciones.py
+++ Proyectos/MontañaRusa/ecuaciones.py
@@ -21,3 +21,21 @@ b = np.array(b,dtype=float)
 def gauss(A, b):
     n = len(b)
     Ab = np.hstack([A, b.reshape(-1, 1)]) #Matriz aumentada en forma A|B
+    
+    for i in range(n):
+        filaMayor = np.argmax(np.abs(Ab[i:, i])) + i
+        Ab[[i, filaMayor]] = Ab[[filaMayor, i]]
+        if Ab[i, i] == 0:
+            raise ValueError("La matriz es singular y no puede resolverse por eliminación de Gauss.")
+        for j in range(i + 1, n):
+            r = Ab[j, i] / Ab[i, i]
+            Ab[j, i:] -= r * Ab[i, i:]
+    x = np.zeros(n)
+    for i in range(n - 1, -1, -1):
+        x[i] = (Ab[i, -1] - np.dot(Ab[i, i + 1:n], x[i + 1:n])) / Ab[i, i]
+    return x.tolist()
+
+#Imprimiendo resultados para comparar
+print("Solución x:")
+print("Con funcion solve:",solve(a,b).tolist())
+print("Con gauss:",gauss(a,b))
