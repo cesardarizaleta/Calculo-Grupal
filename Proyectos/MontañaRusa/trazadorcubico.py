1501b92f622b9a0cd67a6c27cf41b25caed74ec1
JoSantana0005 <joseasantana05@gmail.com>
2024-06-23 13:27:41 -0400

    Agregando la funcion del trazado cubico y agregando las primera derivada

--- Proyectos/MontañaRusa/trazadorcubico.py
+++ Proyectos/MontañaRusa/trazadorcubico.py
@@ -1,5 +1,29 @@
 import api
-#SI NO FUNCIONA CORRE EL ARCHIVO DE API PRIMERO
+import numpy as np
+import sympy as sym
+import matplotlib.pyplot as plt
+
+#Funcion que hace el trazado cubico con frontera Sujeta
+#Condicion : S(x0) = f´(x0) , S(xn) = f´(xn)
+def traza_cubico_(xi,yi):
+    tam = len(xi)
+    h = np.zeros(tam-1,dtype=float) #Valores para h
+    for i in range(0,tam,1):
+        h[i] = xi[i + 1] - xi[i]
+    #Sistema de ecuaciones
+
+    Prim = np.zeros(shape =(tam,tam), dtype=float)
+    Seg = np.array(tam, dtype= float)
+    result = np.array(tam,dtype= float)
+
+    Prim[0,0] = 2*h[0]
+    Prim[0,1] = h[0]
+    Seg[0] = 3*(yi[1] - yi[0])/h[0]
+
+    #Primera derivadas
+    for j in range(0,tam-1,1):
+        
+
 #Extrayendo datos de api
 x,y = api.datos1()
 
