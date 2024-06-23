08735d2740dcb510c710ef47db50828b3f32bcd4
alejandrochmejia <alejandrochmejia@gmail.com>
2024-06-22 21:17:05 -0400

    Empezando resolucion sistema de ecuaciones

--- Proyectos/MontañaRusa/ecuaciones.py
+++ Proyectos/MontañaRusa/ecuaciones.py
@@ -1,4 +1,6 @@
 import api
+import numpy as np
+from scipy.linalg import solve
 
 #Extrayendo datos de la api
 a,b = api.datos4()
@@ -11,5 +13,11 @@ filas = a.split('],[')
 a = []
 for f in filas: a.append([int(x) for x in f.split(',')])
 
-print(a)
-print(b)
+#Convirtiendo matriz y vector a array de numpy
+a = np.array(a,dtype=float)
+b = np.array(b,dtype=float)
+
+#Funcion de eliminacion por gauss
+def gauss(A, b):
+    n = len(b)
+    Ab = np.hstack([A, b.reshape(-1, 1)]) #Matriz aumentada en forma A|B
