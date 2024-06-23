4c674306aea30491800c555fb9d1bc2decae78cd
PepoTH <cesardarizaleta@gmail.com>
2024-06-22 20:17:51 -0400

    Agregando API de prueba

--- Proyectos/MontañaRusa/ortogonales.py
+++ Proyectos/MontañaRusa/ortogonales.py
@@ -2,13 +2,13 @@ import requests
 
 url = "https://mountain-api1.p.rapidapi.com/api/mountains"
 
-querystring = {"name":"Mount Everest"}
 
 headers = {
 	"x-rapidapi-key": "b3c1599eeamsh15f5406714c8819p11548bjsn28eb8925c3bc",
 	"x-rapidapi-host": "mountain-api1.p.rapidapi.com"
 }
 
-response = requests.get(url, headers=headers, params=querystring)
+response = requests.get(url, headers=headers)
 
-print(response.json())
+#Nos retorna un vector con todos los elementos
+print(response.json()[2]['altitude'])
