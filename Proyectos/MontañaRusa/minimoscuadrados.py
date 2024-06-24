001a0e32a7c310ee00617226048165774f28c228
JoSantana0005 <joseasantana05@gmail.com>
2024-06-23 22:26:17 -0400

    Se agrega la funcion que evalua en el polinomio y la funcion que devuelve el polinomio en una funcion

--- Proyectos/MontañaRusa/minimoscuadrados.py
+++ Proyectos/MontañaRusa/minimoscuadrados.py
@@ -6,6 +6,16 @@ import matplotlib.pyplot as plt
 def calculo_coeficiente(xi,yi):
     coef = np.polyfit(xi,yi,3)
     return coef
+#Funcion que arregla que evalua el polinomio y devuelve los valores previstos
+def evaluacion_polinomio(poli,xi):
+    eval = np.polyval(poli,xi)
+    return eval
+#Funcion que devuelve el polinomio en una expresion
+def polinomio(coef):
+    X = sym.symbols("x")
+    funcion = coef[0]*(X**3) + coef[1]*(X**2) + coef[2]*(X) + coef[3]
+    return funcion
+
 x,y = api.datos2()
 
 x = x.strip('][').split(',')
