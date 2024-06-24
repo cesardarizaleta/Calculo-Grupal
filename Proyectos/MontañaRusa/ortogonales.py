ccffd5062d480f0985290d4bd3a7e27570453cf3
PepoTH <cesardarizaleta@gmail.com>
2024-06-24 14:01:54 -0400

    Arreglos

--- Proyectos/MontañaRusa/ortogonales.py
+++ Proyectos/MontañaRusa/ortogonales.py
@@ -17,11 +17,11 @@ def rodrigues(n,xi):
     #Evalf evalua la funcion de rodri
     return rodri.evalf(subs={x: xi})
 
-def hermite(n):
+#Metodo hermite
+def hermite(n,xi):
     x = sy.symbols('x')
-    h = ((-1)**n)*(math.e**((x**2)/2))*derivar(n,(math.e**((-(x**2))/2)))
-    return h
-    pass
+    h = ((-1)**n)*(math.e**((x**2)/2))*derivar(n,(math.e**((-x**2)/2)))
+    return h.evalf(subs={x: xi})
     
 x, y = api.datos1()
 
@@ -31,14 +31,24 @@ for i in range(len(x)): x[i] = float(x[i])
 y = y.strip('][').split(',')
 for i in range(len(y)): y[i] = float(y[i])
 
-n = 5  # orden del polinomio
+n = 2  # orden del polinomio
+plt.plot(x,y)
 
 #Polinomio Rodrigues
 #Dominio de Lagendre es [-1,1]
 #x = np.linspace(-1,1, 100)
 #y = [rodrigues(n,xi) for xi in x]
 
-print(hermite(0))
+#for i in range(0,len(x)-1):
+    #intervalo = np.linspace(x[i],x[i+1], 10)
+    #y = [hermite(n,xi) for xi in intervalo]
+    #plt.plot(intervalo,y)
+    #pass
+
+#plt.plot([hermite(n,xi) for xi in x])
+
+#x = np.linspace(-4,6, 100)
+#y = [hermite(n,xi) for xi in x]
 
 #plt.plot(x,y)
-#plt.show()
+plt.show()
