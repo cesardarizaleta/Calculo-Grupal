e911a6131c2e5bc7882b778fdf847669aaa6b5af
PepoTH <cesardarizaleta@gmail.com>
2024-06-24 23:53:09 -0400

    Agregando Documentaciones y  Listo

--- Proyectos/MontañaRusa/ortogonales.py
+++ Proyectos/MontañaRusa/ortogonales.py
@@ -2,13 +2,13 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # Función de Legendre
-def legendre(n, x):  
+def l(n, x):  
     if n == 0:  
         return 1  
     elif n == 1:  
         return x  
     else:  
-        return ((2*n-1)*x*legendre(n-1, x) - (n-1)*legendre(n-2, x)) / n  
+        return ((2*n-1)*x*l(n-1, x) - (n-1)*l(n-2, x)) / n  
 
 # Datos
 x_data = np.array([0, 1, 2, 3, 4, 5])
@@ -20,16 +20,18 @@ grado = 6
 # Crear matriz de polinomios de Legendre
 A = np.zeros((len(x_data), grado + 1))
 for i in range(grado + 1):
-    A[:, i] = np.array([legendre(i, x) for x in x_data])
+    A[:, i] = np.array([l(i, x) for x in x_data])
 
 # Encontrar coeficientes que mejor se ajustan a los datos
+#Cabe Recalcar que rcond es porque si no lo colocas salta un warning
 coeficientes = np.linalg.lstsq(A, y_data, rcond=None)[0]
+print(coeficientes)
 
 # Calcular valores para graficar la curva ajustada
 x_plot = np.linspace(x_data.min(), x_data.max(), 100)
 y_plot = np.zeros(len(x_plot))
 for i in range(grado + 1):
-    y_plot += coeficientes[i] * np.array([legendre(i, x) for x in x_plot])
+    y_plot += coeficientes[i] * np.array([l(i, x) for x in x_plot])
 
 # Graficar
 plt.plot(x_plot, y_plot)
