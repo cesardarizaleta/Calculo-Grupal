247f4c3e19201b580a8d1f5393f8204d778b7ca0
PepoTH <cesardarizaleta@gmail.com>
2024-06-24 20:56:54 -0400

    Arreglos => Organizacion

--- Proyectos/MontañaRusa/ortogonales.py
+++ Proyectos/MontañaRusa/ortogonales.py
@@ -4,6 +4,21 @@ import sympy as sy
 import matplotlib.pyplot as plt
 import api
 
+
+#Recoleccion de Datos de la Api
+x, y = api.datos1()
+
+x = x.strip('][').split(',')
+for i in range(len(x)): x[i] = float(x[i])
+
+y = y.strip('][').split(',')
+for i in range(len(y)): y[i] = float(y[i])
+#Datos:
+#x = np.array([0, 1, 2, 3, 4, 5])
+#y = np.array([0.5, 0.8, 1.0, 0.9, 1.2, 0.7])
+
+print(x,y)
+
 #Metodo para derivar enesimamente
 def derivar(n,expresion,x=sy.symbols("x")):
     for i in range(0,n):
@@ -22,33 +37,9 @@ def hermite(n,xi):
     x = sy.symbols('x')
     h = ((-1)**n)*(math.e**((x**2)/2))*derivar(n,(math.e**((-x**2)/2)))
     return h.evalf(subs={x: xi})
-    
-x, y = api.datos1()
 
-x = x.strip('][').split(',')
-for i in range(len(x)): x[i] = float(x[i])
-
-y = y.strip('][').split(',')
-for i in range(len(y)): y[i] = float(y[i])
 
 n = 2  # orden del polinomio
-plt.plot(x,y)
-
-#Polinomio Rodrigues
-#Dominio de Lagendre es [-1,1]
-#x = np.linspace(-1,1, 100)
-#y = [rodrigues(n,xi) for xi in x]
-
-#for i in range(0,len(x)-1):
-    #intervalo = np.linspace(x[i],x[i+1], 10)
-    #y = [hermite(n,xi) for xi in intervalo]
-    #plt.plot(intervalo,y)
-    #pass
-
-#plt.plot([hermite(n,xi) for xi in x])
-
-#x = np.linspace(-4,6, 100)
-#y = [hermite(n,xi) for xi in x]
 
 #plt.plot(x,y)
 plt.show()
