0a614c4ec10818a418f3888bb0e888021ce21632
PepoTH <cesardarizaleta@gmail.com>
2024-06-24 23:35:43 -0400

    Agregando Grado

--- Proyectos/MontañaRusa/ortogonales.py
+++ Proyectos/MontañaRusa/ortogonales.py
@@ -1,45 +1,23 @@
-import numpy as np 
-import math
-import sympy as sy
+import numpy as np
 import matplotlib.pyplot as plt
-import api
 
+# Función de Legendre
+def legendre(n, x):  
+    if n == 0:  
+        return 1  
+    elif n == 1:  
+        return x  
+    else:  
+        return ((2*n-1)*x*legendre(n-1, x) - (n-1)*legendre(n-2, x)) / n  
 
-#Recoleccion de Datos de la Api
-x, y = api.datos1()
+# Datos
+x_data = np.array([0, 1, 2, 3, 4, 5])
+y_data = np.array([0.5, 0.8, 1.0, 0.9, 1.2, 0.7])
 
-x = x.strip('][').split(',')
-for i in range(len(x)): x[i] = float(x[i])
+# Grado del polinomio
+grado = 6
 
-y = y.strip('][').split(',')
-for i in range(len(y)): y[i] = float(y[i])
-#Datos:
-#x = np.array([0, 1, 2, 3, 4, 5])
-#y = np.array([0.5, 0.8, 1.0, 0.9, 1.2, 0.7])
-
-print(x,y)
-
-#Metodo para derivar enesimamente
-def derivar(n,expresion,x=sy.symbols("x")):
-    for i in range(0,n):
-        expresion = sy.diff(expresion,x)
-    return expresion
-
-#Metodo rodrigues para realizar legendre
-def rodrigues(n,xi):
-    x = sy.symbols('x')
-    rodri = (1/((2**n) * math.factorial(n))) * derivar(n,(x**2 - 1)**n)
-    #Evalf evalua la funcion de rodri
-    return rodri.evalf(subs={x: xi})
-
-#Metodo hermite
-def hermite(n,xi):
-    x = sy.symbols('x')
-    h = ((-1)**n)*(math.e**((x**2)/2))*derivar(n,(math.e**((-x**2)/2)))
-    return h.evalf(subs={x: xi})
-
-
-n = 2  # orden del polinomio
-
-#plt.plot(x,y)
-plt.show()
+# Crear matriz de polinomios de Legendre
+A = np.zeros((len(x_data), grado + 1))
+for i in range(grado + 1):
+    A[:, i] = np.array([legendre(i, x) for x in x_data])
